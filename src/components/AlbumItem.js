3de1e8de4512653a193e5a71b9618006d82c4bfd
ah-james <11ahj11@gmail.com>
2022-09-03 18:27:37 -0400

    AlbumItem basic CSS started

--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,6 +1,17 @@
+import './AlbumItem.css'
+
 const AlbumItem = props => {
     return(
-        <p>Album item</p>
+        <div className="album-item">
+            <div className="album-item-description">
+                <h4>Artist Name</h4>
+                <h4>Album Title</h4>
+            </div>
+            <div className="album-item-info">
+                <p>release year</p>
+                <p>rating</p>
+            </div>
+        </div>
     )
 }
 
