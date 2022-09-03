9c255a90f5b561e0e8bdb1150230ffe0c45fb9b1
ah-james <11ahj11@gmail.com>
2022-09-03 18:10:34 -0400

    Adding AlbumItem component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import AlbumItem from './components/AlbumItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bouncr</h1>
+      <h3>Social Media for Music Fans</h3>
+      <AlbumItem />
     </div>
   );
 }
---
+++ src/components/AlbumItem.js
@@ -0,0 +1,7 @@
+const AlbumItem = props => {
+    return(
+        <p>Album item</p>
+    )
+}
+
+export default AlbumItem
