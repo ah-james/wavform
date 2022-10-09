fc1e67384871e7ae14571396ec6372a4457487a1
ah-james <11ahj11@gmail.com>
2022-10-08 21:21:35 -0400

    Adding form state and onChange props to form fields

--- src/components/AlbumForm.js
+++ src/components/AlbumForm.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './AlbumForm.css'
 
 const AlbumForm = props => {
+    // manage state
+    const [artist, setArtist] = useState('')
+    const [album, setAlbum] = useState('')
+    const [rating, setRating] = useState('')
+    const [date, setDate] = useState('')
+
+    const handleArtistChange = (event) => {
+        setArtist(event.target.value)
+    }
+
+    const handleAlbumChange = event => {
+        setAlbum(event.target.value)
+    }
+
+    const handleRatingChange = (event) => {
+        setRating(event.target.value)
+    }
+
+    const handleDateChange = (event) => {
+        setDate(event.target.value)
+    }
+
     return(
         <form>
-            <div className="new-expense-controls">
-                <div className="new-expense-control">
+            <div className="new-album-controls">
+                <div className="new-album-control">
                     <label>Artist</label>
-                    <input type='text' />
+                    <input type='text' onChange={handleArtistChange} />
                 </div>
-                <div className="new-expense-control">
+                <div className="new-album-control">
                     <label>Album</label>
-                    <input type='text' />
+                    <input type='text' onChange={handleAlbumChange} />
                 </div>
-                <div className="new-expense-control">
+                <div className="new-album-control">
                     <label>Rating</label>
-                    <input type='text' />
+                    <input type='text' onChange={handleRatingChange} />
                 </div>
-                <div className="new-expense-control">
+                <div className="new-album-control">
                     <label>Date</label>
-                    <input type='date' />
+                    <input type='date' onChange={handleDateChange} />
                 </div>
             </div>
-            <div className="new-expense-actions">
-                <button type="submit">Add Expense</button>
+            <div className="new-album-actions">
+                <button type="submit">Add Album</button>
             </div>
         </form>
     )
