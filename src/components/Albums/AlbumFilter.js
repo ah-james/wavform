5c81ea17cc5e6ca5d9347b9aeaa3dd3b071830d4
ah-james <11ahj11@gmail.com>
2022-10-22 13:38:13 -0400

    Album Filter component now filters out for selected date

--- src/components/Albums/AlbumFilter.js
+++ src/components/Albums/AlbumFilter.js
@@ -12,6 +12,7 @@ const AlbumFilter = props => {
             <div className='album-filter-control'>
                 <label>Filter by Year</label>
                 <select value={props.selectedYear} onChange={handleChange}>
+                    <option value=''></option>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -7,19 +7,27 @@ import AlbumItem from '../Albums/AlbumItem';
 import Card from '../UI/Card';
 
 const Albums = props => {
-    const [filteredYear, setFilteredYear] = useState()
+    const [filteredYear, setFilteredYear] = useState('')
 
     const handleFilterChange = selectedYear => {
         setFilteredYear(selectedYear)
     }
 
+    const filteredAlbums = props.reviews.filter(review => {
+        if (filteredYear === '') {
+            return props.reviews
+        }
+        return review.date.getFullYear().toString() === filteredYear
+    })
+
     return(
         <Card className='albums'>
             <AlbumFilter selectedYear={filteredYear} handleFilterChange={handleFilterChange}/>
-            {props.reviews.map((review) => <AlbumItem key={review.id} artist={review.artist} album={review.album} date={review.date} rating={review.rating} />)}
+            {filteredAlbums.map((filteredReview) => 
+                <AlbumItem key={filteredReview.id} artist={filteredReview.artist} album={filteredReview.album} date={filteredReview.date} rating={filteredReview.rating} />
+            )}
         </Card>
     )
-
 }
 
 export default Albums
