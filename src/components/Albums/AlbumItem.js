a6818b088a54f6ec372ff8ca2c25a0faffa8dd19
ah-james <11ahj11@gmail.com>
2022-11-09 20:02:58 -0500

    Some lil list stylings and tiny changes

--- src/components/Albums/AlbumItem.js
+++ src/components/Albums/AlbumItem.js
@@ -8,20 +8,22 @@ const AlbumItem = props => {
     const year = props.date.getFullYear()
 
     return(
-        <Card className="album-item">
-            <div className="album-description">
-                <div className="album-month">{month}</div>
-                <div className="album-day">{day}</div>
-                <div className="album-year">{year}</div>
-            </div>
-            <div className="album-item-description">
-                <h2>{props.artist}</h2>
-                <h2>{props.album}</h2>
-            </div>
-            <div className="album-item-info">
-                <p>{props.rating}/10</p>
-            </div>
-        </Card>
+        <li>
+            <Card className="album-item">
+                <div className="album-description">
+                    <div className="album-month">{month}</div>
+                    <div className="album-day">{day}</div>
+                    <div className="album-year">{year}</div>
+                </div>
+                <div className="album-item-description">
+                    <h2>{props.artist}</h2>
+                    <h2>{props.album}</h2>
+                </div>
+                <div className="album-item-info">
+                    <p>{props.rating}/10</p>
+                </div>
+            </Card>
+        </li>
     )
 }
 
