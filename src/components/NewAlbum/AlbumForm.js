e68a111a5a2481663e377d69409fe1f5a564aeef
ah-james <11ahj11@gmail.com>
2022-11-12 15:21:55 -0500

    Conditional rendering of album form component

--- src/components/NewAlbum/AlbumForm.js
+++ src/components/NewAlbum/AlbumForm.js
@@ -8,6 +8,7 @@ const AlbumForm = props => {
     const [album, setAlbum] = useState('')
     const [rating, setRating] = useState('')
     const [date, setDate] = useState('')
+    const [form, setForm] = useState(false)
 
     const handleArtistChange = (event) => {
         setArtist(event.target.value)
@@ -42,32 +43,45 @@ const AlbumForm = props => {
         setAlbum('')
         setRating('')
         setDate('')
+        setForm(current => !current)
     }
 
-    return(
-        <form onSubmit={handleSubmit} >
-            <div className="new-album-controls">
-                <div className="new-album-control">
-                    <label>Artist</label>
-                    <input type='text' value={artist} onChange={handleArtistChange} />
+    const handleClick = () => {
+        setForm(current => !current)
+    }
+
+    if (form) {
+        return(
+            <form onSubmit={handleSubmit} >
+                <div className="new-album-controls">
+                    <div className="new-album-control">
+                        <label>Artist</label>
+                        <input type='text' value={artist} onChange={handleArtistChange} />
+                    </div>
+                    <div className="new-album-control">
+                        <label>Album</label>
+                        <input type='text' value={album} onChange={handleAlbumChange} />
+                    </div>
+                    <div className="new-album-control">
+                        <label>Rating</label>
+                        <input type='number' min='0' max='10' value={rating} onChange={handleRatingChange} />
+                    </div>
+                    <div className="new-album-control">
+                        <label>Date</label>
+                        <input type='date' value={date} onChange={handleDateChange} />
+                    </div>
                 </div>
-                <div className="new-album-control">
-                    <label>Album</label>
-                    <input type='text' value={album} onChange={handleAlbumChange} />
+                <div className="new-album-actions">
+                    <button onClick={handleClick}>Cancel</button>
+                    <button type="submit">Add Album</button>
                 </div>
-                <div className="new-album-control">
-                    <label>Rating</label>
-                    <input type='number' min='0' max='10' value={rating} onChange={handleRatingChange} />
-                </div>
-                <div className="new-album-control">
-                    <label>Date</label>
-                    <input type='date' value={date} onChange={handleDateChange} />
-                </div>
-            </div>
-            <div className="new-album-actions">
-                <button type="submit">Add Album</button>
-            </div>
-        </form>
+            </form>
+        )
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>Add an Album</button>
+        </div>
     )
 }
 
