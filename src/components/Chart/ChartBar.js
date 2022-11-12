84115fd346a7e543d0bf0f4ed74bf0c161beccab
ah-james <11ahj11@gmail.com>
2022-11-12 17:42:30 -0500

    Building out basic chart structure to show how many albums from each month are listened to

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,25 @@
+
+const ChartBar = props => {
+    // calculate bar fill height with variable that will help set CSS
+    let barFill = '0%'
+
+    // if max value from props is higher than zero convert to percentage and save as barFill
+    // also convert to string with % sign at end
+    if (props.maxValue > 0) {
+        barFill = Math.round((props.value / props.maxValue) * 100 ) + '%'
+    }
+
+    return(
+        <div className="chart-bar">
+            <div className="chart-bar-inner">
+                {/* dynamically adjust height with barFill variable in style react prop */}
+                {/* value is a javascript object */}
+                <div className="chart-bar-fill" style={{height: barFill}} ></div>
+            </div>
+            {/* put label from props in this one */}
+            <div className="chart-bar-label">{props.label}</div>
+        </div>
+    )
+}
+
+export default ChartBar
