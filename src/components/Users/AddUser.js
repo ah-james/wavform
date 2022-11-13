6bccb7e69493d32701c5a8bc8f5102b40017e1ea
ah-james <11ahj11@gmail.com>
2022-11-13 15:04:50 -0500

    Basic form functionality for AddUser, including beginnings of form validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,36 @@
+import { useState } from "react"
 import Card from "../UI/Card"
 import styles from './AddUser.module.css'
 
 const AddUser = props => {
+    // usestate for username, email, password
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     // function to handle add user
     const handleAddUser = event => {
         event.preventDefault()
+        if (username.length === 0 || email.length === 0 || password.length === 0) {
+            return
+        }
+        console.log(username, email, password)
+        setUsername('')
+        setEmail('')
+        setPassword('')
+    }
+
+    // handle changes for username, email, password
+    const handleUsernameChange = event => {
+        setUsername(event.target.value)
+    }
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value)
     }
 
     // return form for username and password
@@ -14,13 +40,13 @@ const AddUser = props => {
             <form onSubmit={handleAddUser}>
                 {/* label and input for email */}
                 <label htmlFor="email">Email</label>
-                <input id='email' type='email' />
+                <input id='email' type='email' value={email} onChange={handleEmailChange} />
                 {/* label and input for username */}
                 <label htmlFor="username">Username</label>
-                <input id='username' />
+                <input id='username' value={username} onChange={handleUsernameChange} />
                 {/* label and input for password */}
                 <label>Password</label>
-                <input id='password' type='password' />
+                <input id='password' value={password} type='password' onChange={handlePasswordChange} />
                 {/* button for submit */}
                 <button type="submit">Create</button>
             </form>
