f4afb7118637838886c4b5dd12e73ea527228979
ah-james <11ahj11@gmail.com>
2022-11-16 22:03:41 -0500

    little cleanup in Login

--- src/components/Users/Login.js
+++ src/components/Users/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
-
+// finish this when I have the router up and running
+// 
 const Login = props => {
     // username and password state
     const [username, setUsername] = useState('')
@@ -14,14 +15,19 @@ const Login = props => {
         setPassword(event.target.value)
     }
 
+    const handleLoginSubmit = (event) => {
+        event.preventDefault()
+        // prop to handle user login
+    }
+
     return(
-        <Card className={styles.input}>
-            <form onSubmit={handleAddUser}>
+        <Card>
+            <form onSubmit={handleLoginSubmit}>
                 {/* label and input for username */}
                 <label htmlFor="username">Username</label>
-                <input id='username' value={username} type='email' onChange={handleUsernameChange} />
+                <input id='username' value={username} onChange={handleUsernameChange} />
                 {/* label and input for password */}
-                <label>Password</label>
+                <label htmlFor="password">Password</label>
                 <input id='password' value={password} type='password' onChange={handlePasswordChange} />
                 {/* button for submit */}
                 <button type="submit">Login</button>
