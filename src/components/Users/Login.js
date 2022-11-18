52c1a2bd9221cbf2046185480dd1e4f4054dc1e2
ah-james <11ahj11@gmail.com>
2022-11-17 21:55:45 -0500

    Adding validations and useEffect hook to login form

--- src/components/Users/Login.js
+++ src/components/Users/Login.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "../UI/Card"
 import styles from './Login.module.css'
 
@@ -8,6 +8,16 @@ const Login = props => {
     // username and password state
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [validUsername, setValidUsername] = useState()
+    const [validPassword, setValidPassword] = useState()
+    const [validForm, setValidForm] = useState(false)
+
+    // add useEffect to handle form validations
+    useEffect(() => {
+        setValidForm(
+            username.length > 6 && password.length > 6
+        )
+    }, [username, password])
 
     const handleUsernameChange = event => {
         setUsername(event.target.value)
@@ -16,6 +26,14 @@ const Login = props => {
     const handlePasswordChange = event => {
         setPassword(event.target.value)
     }
+
+    const handleValidateUsername = () => {
+        setValidUsername(username.length > 6);
+      };
+    
+      const handleValidatePassword = () => {
+        setValidPassword(password.length > 6);
+      };
 
     const handleLoginSubmit = (event) => {
         event.preventDefault()
@@ -26,20 +44,19 @@ const Login = props => {
     return(
         <Card className={styles.login}>
             <form onSubmit={handleLoginSubmit}>
-                {/* <div className={ `${styles.control} ${emailIsValid === false ? styles.invalid : ''}` } /> */}
-                <div className={styles.control}>
+                <div className={`${styles.control} ${validUsername === false ? styles.invalid : ''}`}>
                     {/* label and input for username */}
                     <label htmlFor="username">Username</label>
-                    <input id='username' value={username} onChange={handleUsernameChange} />
+                    <input id='username' value={username} onChange={handleUsernameChange} onBlur={handleValidateUsername} />
                 </div>
-                <div className={styles.control}>
+                <div className={`${styles.control} ${validPassword === false ? styles.invalid : ''}`}>
                     {/* label and input for password */}
                     <label htmlFor="password">Password</label>
-                    <input id='password' value={password} type='password' onChange={handlePasswordChange} />
+                    <input id='password' value={password} type='password' onChange={handlePasswordChange} onBlur={handleValidatePassword} />
                 </div>
                 <div className={styles.actions}>
                     {/* button for submit */}
-                    <button type="submit">Login</button>
+                    <button type="submit" disabled={!validForm}>Login</button>
                 </div>
             </form>
         </Card>
