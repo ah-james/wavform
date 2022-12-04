e26d140dd3929f0eaa8160ed5fa6b6239ce4a609
ah-james <11ahj11@gmail.com>
2022-12-04 10:01:26 -0500

    HTTP request error handling in search container

--- src/containers/ArtistSearchContainer.js
+++ src/containers/ArtistSearchContainer.js
@@ -8,6 +8,8 @@ const ArtistSearchContainer = props => {
     const [albums, setAlbums] = useState([])
     // bit of state to check if content is currently loading, starting false
     const [loading, setLoading] = useState(false)
+    // error state to handle HTTP errors
+    const [error, setError] = useState(null)
 
     // Spotify API requires specific parameters for an access token to use their API, run that when component loads
     useEffect(() => {
@@ -30,6 +32,7 @@ const ArtistSearchContainer = props => {
         event.preventDefault()
         // setLoading to true
         setLoading(true)
+        // reset errors to null
 
         const searchParameters = {
             method: 'GET',
@@ -39,17 +42,28 @@ const ArtistSearchContainer = props => {
             }
         }
 
-        // GET an artist via search
-        const response = await fetch(`https://api.spotify.com/v1/search?q=${search}&type=artist`, searchParameters)
-        const artistData = await response.json()
-        // want to eventually have a suggestion box that autofills when user starts typing, for now use top artist ID
-        // ID not name to add to string to find artist's albums
-        const artistID =  artistData.artists.items[0].id
+        try {
+            // GET an artist via search
+            const response = await fetch(`https://api.spotify.com/v1/search?q=${search}&type=artist`, searchParameters)
 
-        // GET all albums by an artist
-        const returnedAlbums = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limit=50`, searchParameters)
-        const albumResponse = await returnedAlbums.json()
-        setAlbums(albumResponse.items)
+            // if the response fails throw a new error
+            if (!response.ok) {
+                throw new Error('Something went wrong')
+            }
+            const artistData = await response.json()
+
+            // want to eventually have a suggestion box that autofills when user starts typing, for now use top artist ID
+            // ID not name to add to string to find artist's albums
+            const artistID =  artistData.artists.items[0].id
+
+            // GET all albums by an artist
+            const returnedAlbums = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limit=50`, searchParameters)
+            const albumResponse = await returnedAlbums.json()
+            setAlbums(albumResponse.items)
+
+        } catch (error) {
+            setError(error.message)
+        }
         // setLoading to false
         setLoading(false)
     }
@@ -59,10 +73,11 @@ const ArtistSearchContainer = props => {
         <form onSubmit={searchArtists}>
             <input type='text' onChange={event => setSearch(event.target.value)}></input>
             <button type='submit'>Search</button>
-            {!loading && albums.length === 0 && <p>No Albums Found</p>}
+            {!loading && albums.length === 0 && !error && <p>No Albums Found</p>}
             {loading && <p>Loading...</p>}
+            {!loading && error && <p>{error}</p>}
             {albums.map(album => 
-                <div>
+                <div key={album.id}>
                     <p>{album.name}</p>
                     <img src={album.images[1].url}></img> 
                 </div>
