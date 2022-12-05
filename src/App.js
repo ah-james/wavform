2971f5d93d20fb4a9fba80b5981f8af99600def9
ah-james <11ahj11@gmail.com>
2022-12-04 21:16:18 -0500

    Adding Firebase connection for Albums database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import './App.css';
 
 import NewAlbum from './components/NewAlbum/NewAlbum';
@@ -8,7 +8,7 @@ import Home from './components/Users/Home';
 import Header from './components/Header/Header';
 import AuthContext from './store/auth-context';
 
-import ArtistSearchContainer from './containers/ArtistSearchContainer';
+// import ArtistSearchContainer from './containers/ArtistSearchContainer';
 
 const DUMMY_DATA = [
   {
@@ -28,26 +28,48 @@ const DUMMY_DATA = [
 ]
 
 function App() {
-  // const [reviews, setReviews] = useState(DUMMY_DATA)
+  const [reviews, setReviews] = useState([])
 
-  // // useContext hook to manage state 
-  // const ctx = useContext(AuthContext)
+  const handleFetchReviews = useCallback(async () => {
+    const response = await fetch('https://react-bouncr-default-rtdb.firebaseio.com/reviews.json')
+    const data = await response.json()
+    const loadedReviews = []
 
-  // const handleAddAlbum = album => {
-  //   setReviews((prevReviews) => {
-  //     return [album, ...prevReviews]
-  //   })
-  // }
+    for (const key in data) {
+      loadedReviews.push({
+        id: key,
+        artist: data[key].artist,
+        album: data[key].album,
+        date: data[key].date,
+        rating: data[key].rating
+      })
+    }
+
+    setReviews(loadedReviews)
+  }, [])
+
+  useEffect(() => {
+    handleFetchReviews()
+  }, [handleFetchReviews])
+
+  // useContext hook to manage state 
+  const ctx = useContext(AuthContext)
+
+  const handleAddReview = review => {
+    setReviews((prevReviews) => {
+      return [review, ...prevReviews]
+    })
+  }
 
   return (
     <React.Fragment>
-      {/* <Header />
-      <NewAlbum onAddAlbum={handleAddAlbum} />
-      <Albums reviews={reviews} /> */}
+      <Header />
+      <NewAlbum onAddReview={handleAddReview} />
+      <Albums reviews={reviews} />
       {/* managing handleLogin & logout functions in auth context now */}
-      {/* {!ctx.loggedIn && <Login />}
-      {ctx.loggedIn && <Home />} */}
-      <ArtistSearchContainer />
+      {!ctx.loggedIn && <Login />}
+      {ctx.loggedIn && <Home />}
+      {/* <ArtistSearchContainer /> */}
     </React.Fragment>
   );
 }
