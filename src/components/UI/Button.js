e9b9d62b3cd318eb6a4da1851124a80b128fbf38
ah-james <11ahj11@gmail.com>
2022-12-09 22:53:39 -0500

    CSS styling for disabled button in custom button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,13 @@ import styles from './Button.module.css'
 
 const Button = props => {
     return(
-        <button className={styles.button} type={props.type} onClick={props.handleClick} disabled={props.disabled}>{props.children}</button>
+        <button 
+        className={`${styles.button} ${props.disabled === true ? styles.disabled : ''}`} 
+        type={props.type} 
+        onClick={props.handleClick} 
+        disabled={props.disabled}>
+            {props.children}
+        </button>
     )
 }
 
