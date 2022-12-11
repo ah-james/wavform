a0df9a990af17d3947f27656fe8c94ee72a11461
ah-james <11ahj11@gmail.com>
2022-12-11 11:16:55 -0500

    reformatting store

--- src/store/actions/reviewsActions.js
+++
@@ -1,7 +0,0 @@
-export const fetchReviews = () => {
-    // move fetchReviews from app.js to here
-}
-
-export const addReview = () => {
-    // move addReview from form to here
-}
--- src/store/reducers/reviewsReducer.js
+++
@@ -1,3 +0,0 @@
-const reviewsReducer = (state = [], action) => {
-    // add GET_REVIEWS and ADD_REVIEW to start
-}
