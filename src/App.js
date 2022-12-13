18474682ee9b744e83b62892ba07d6de4c25ef45
ah-james <11ahj11@gmail.com>
2022-12-12 23:57:55 -0500

    Hooking up React Router to app

--- src/App.js
+++ src/App.js
@@ -1,59 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
 import './App.css';
 
-import NewAlbum from './components/NewReview/NewReview';
-import Reviews from './components/Reviews/Reviews';
-// import Login from './components/Users/Login'
-// import Home from './components/Users/Home';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { Route } from 'react-router-dom'
+
 import Header from './components/Header/Header';
-import styles from './components/Reviews/ReviewsList.module.css'
 import { fetchReviews } from './store/reviews-actions';
+import HomeContainer from './containers/HomeContainer';
+import LoginContainer from './containers/LoginContainer';
 
 // import ArtistSearchContainer from './containers/ArtistSearchContainer';
 
 function App() {
-
   const dispatch = useDispatch()
-  const reviews = useSelector((state) => {
-    return state.reviews.albums
-  })
 
   useEffect(() => {
     dispatch(fetchReviews())
   }, [dispatch])
 
-  // need to use this when updating state after posting because state won't rerender without it
-  const handleAddReview = review => {
-    // setReviews((prevReviews) => prevReviews.concat(review))
-  }
-
-  let content = <p className={styles["reviews-list-fallback"]}>Loading...</p>
-
-  // I'll figure this out someday \/
-
-  // if (error) {
-  //   content = <p className={styles["reviews-list-fallback"]}>{error}</p>
-  // }
-
-  // if (loading) {
-  //   content = <p className={styles["reviews-list-fallback"]}>Loading...</p>
-  // }
-
-  if (reviews.length > 0) {
-    content = <Reviews reviews={reviews} />
-  }
-
   return (
-    <React.Fragment>
+    <div>
       <Header />
-      <NewAlbum onAddReview={handleAddReview} />
-      {content}
-      {/* managing handleLogin & logout functions in auth context now */}
-      {/* {!ctx.loggedIn && <Login />}
-      {ctx.loggedIn && <Home />} */}
+      <Route path='/home'>
+        <HomeContainer />
+      </Route>
+      <Route path='/login'>
+        <LoginContainer />
+      </Route>
       {/* <ArtistSearchContainer /> */}
-    </React.Fragment>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store/index';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // wrap entire app in AuthContextProvider state management
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+
 
 );
 
