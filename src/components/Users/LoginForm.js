75c1711a3c689207607eb87939ea6c14efb81a80
ah-james <11ahj11@gmail.com>
2022-12-16 23:39:08 -0500

    Login form now changes to signup

--- src/components/Users/LoginForm.js
+++ src/components/Users/LoginForm.js
@@ -34,6 +34,7 @@ const reducer = (state, action) => {
 
 const Login = props => {
     const [validForm, setValidForm] = useState(false)
+    const [newAccount, setNewAccount] = useState(false)
 
     const navigate = useNavigate()
 
@@ -42,6 +43,8 @@ const Login = props => {
 
     const [passwordState, dispatchPassword] = useReducer(reducer, initialState)
 
+    const [emailState, dispatchEmail] = useReducer(reducer, initialState)
+
     // useContext to manage state
 
     // pull out isValid property and save them as constants to use in useEffect
@@ -49,13 +52,14 @@ const Login = props => {
 
     const { isValid: userIsValid } = userState
     const { isValid: passwordIsValid } = passwordState
+    const { isValid: emailIsValid } = emailState
 
     // add useEffect to handle form validations
     useEffect(() => {
         // I think I have a memory leak and I've been told this might help I'm sorry for what I'm about to do
         const identifier = setTimeout(() => {
             setValidForm(
-                userIsValid && passwordIsValid
+                userIsValid && passwordIsValid && emailIsValid
             )
         }, 500)
         return () => {
@@ -63,25 +67,33 @@ const Login = props => {
         }
 
         // don't use userState etc objects in dependencies, effect function will rerun whenever any property of this changes
-    }, [userIsValid, passwordIsValid])
+    }, [userIsValid, passwordIsValid, emailIsValid])
 
     const handleUsernameChange = event => {
         // dispatchUsername with keys of type (string explaining what's happening) and value (event.target.value)
-        dispatchUser({type: "INPUT_CHANGE", value: event.target.value})
+        dispatchUser({ type: "INPUT_CHANGE", value: event.target.value })
     }
 
     const handlePasswordChange = event => {
-        dispatchPassword({type: 'INPUT_CHANGE', value: event.target.value})
+        dispatchPassword({ type: 'INPUT_CHANGE', value: event.target.value })
+    }
+
+    const handleEmailChange = event => {
+        dispatchEmail({ type: 'INPUT_CHANGE', value: event.target.value })
     }
 
     const handleValidateUsername = () => {
         // dispatchUsername with keys of type (string explaining what's happening for reducer)
-        dispatchUser({type: 'INPUT_BLUR'});
+        dispatchUser({ type: 'INPUT_BLUR' });
     };
     
-      const handleValidatePassword = () => {
-        dispatchPassword({type: 'INPUT_BLUR'});
+    const handleValidatePassword = () => {
+        dispatchPassword({ type: 'INPUT_BLUR' });
     };
+
+    const handleValidateEmail = () => {
+        dispatchEmail({ type: 'INPUT_BLUR' })
+    }
 
     const handleLoginSubmit = (event) => {
         event.preventDefault()
@@ -91,10 +103,22 @@ const Login = props => {
         
     }
 
+    const handleFormChange = () => {
+        setNewAccount(current => !current)
+    }
+
+    let email = null
+
+    if (newAccount === true) {
+        email = <Input label="Email" id="email" isValid={emailIsValid} value={emailState.value} onChange={handleEmailChange} onBlur={handleValidateEmail} />
+    }
+
     return(
         <Card className={styles["new-user"]}>
+            <h2>{newAccount ? 'Sign Up' : 'Login'}</h2>
             <form onSubmit={handleLoginSubmit}>
                 <div className={styles.controls}>
+                    {email}
                     <Input label="Username" id="username" isValid={userIsValid} value={userState.value} onChange={handleUsernameChange} onBlur={handleValidateUsername} />
                     <Input label="Password" type="password" id='password' isValid={passwordIsValid} value={passwordState.value} onChange={handlePasswordChange} onBlur={handleValidatePassword} />
                 </div>
@@ -103,6 +127,7 @@ const Login = props => {
                     <Button type='submit' disabled={!validForm}>Login</Button>
                 </div>
             </form>
+            <p onClick={handleFormChange}>{newAccount ? 'Login to your Account' : 'Create New Account'}</p>
         </Card>
     )
 }
