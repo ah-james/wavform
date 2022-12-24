770a89c824369fb1da6193806373fa0024fa3828
ah-james <11ahj11@gmail.com>
2022-12-23 22:48:59 -0500

    Basic Home Page styling

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,10 +1,16 @@
-
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/UI/Button'
+import styles from './HomeContainer.module.css'
 
 const HomeContainer = () => {
+    const navigate = useNavigate()
 
     return(
-        <div>
-            <h2>Home Page</h2>
+        <div className={styles['home-page']}>
+            <h2>Track albums you've listened to.</h2>
+            <h2>Save those you haven't heard yet.</h2>
+            <h2>Tell your friends what's good.</h2>
+            <Button handleClick={() => navigate('/login')}>Get Started - It's Free!</Button>
         </div>
     )
 }
