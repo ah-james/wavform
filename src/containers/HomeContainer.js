a63d26559eab9fa1fd024866d92ddca273df8dee
ah-james <11ahj11@gmail.com>
2022-12-23 22:19:51 -0500

    renaming HomeContainer to ProfileContainer to account for true homescreen that will display when user isn't logged in

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,53 +1,10 @@
-import { useSelector } from 'react-redux'
-
-import NewReview from '../components/NewReview/NewReview';
-import Reviews from '../components/Reviews/Reviews';
-
-import styles from '../components/Reviews/ReviewsList.module.css'
 
 
-// to-do:
-// only show reviews with user ID here
 const HomeContainer = () => {
-
-    const reviews = useSelector((state) => {
-        return state.reviews.albums
-    })
-
-    const user = useSelector((state) => {
-        return state.auth.email
-    })
-
-    let shownReviews = []
-
-    if (user) {
-        for (let i = 0; i < reviews.length; i++) {
-            if (reviews[i]['user'] === user) {
-                shownReviews.push(reviews[i])
-            }
-        }
-    }
-
-    let content = <p className={styles["reviews-list-fallback"]}>Loading...</p>
-
-    // I'll figure this out someday \/
-  
-    // if (error) {
-    //   content = <p className={styles["reviews-list-fallback"]}>{error}</p>
-    // }
-  
-    // if (loading) {
-    //   content = <p className={styles["reviews-list-fallback"]}>Loading...</p>
-    // }
-
-    if (reviews.length > 0) {
-        content = <Reviews reviews={user ? shownReviews : reviews} showChart={true} />
-    }
 
     return(
         <div>
-            <NewReview />
-            {content}
+            <h2>Home Page</h2>
         </div>
     )
 }
