244b73ec34ef85450b419758da09cf91ffe3df39
ah-james <11ahj11@gmail.com>
2022-12-27 21:50:32 -0500

    Renaming ErrorModal component for flexibility

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,54 @@
+// import react dom for createPortal
+import {createPortal} from 'react-dom'
+
+import styles from './Modal.module.css'
+import Card from './Card'
+import Button from './Button'
+
+// new component for portal, overlay div here
+const Backdrop = props => {
+    return <div className={styles.backdrop} onClick={props.handleClick} />
+}
+
+// new component for portal, modal body here
+const ModalOverlay = props => {
+    return (
+        <Card className={styles.modal} >
+            {/* header to contain title of error */}
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            {/* paragraph for message */}
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            {/* footer with confirm button */}
+            <footer className={styles.actions}>
+                <Button type='button' handleClick={props.handleClick}>No</Button>
+                <Button type='button' handleClick={props.handleAction}>Yes</Button>
+            </footer>
+        </Card>
+    )
+}
+
+
+const Modal = props => {
+
+    return(
+        <>
+            {/* createPortal takes in two functions above as JSX elements */}
+            {/* Backdrop needs props.handleError passed in, second arg document.getElementById for backdrop in html file */}
+            {/* same process with Modal, needs all props passed in tho */}
+            {createPortal(
+                <Backdrop handleError={props.handleError} handleClick={props.handleClick} />,
+                document.getElementById('backdrop-root')
+            )}
+            {createPortal(
+                <ModalOverlay title={props.title} message={props.message} handleAction={props.handleAction} handleClick={props.handleClick} />,
+                document.getElementById('overlay-root')
+            )}
+        </>
+    )
+}
+
+export default Modal
