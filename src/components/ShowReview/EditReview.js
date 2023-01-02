62c41610f426a1eac837083830d9ece2d9f1b39a
ah-james <11ahj11@gmail.com>
2023-01-02 15:42:52 -0500

    adding error handling to edit review form

--- src/components/ShowReview/EditReview.js
+++ src/components/ShowReview/EditReview.js
@@ -1,55 +1,57 @@
+import { useState } from 'react'
 import useInput from '../../hooks/use-input'
 import Card from '../UI/Card'
 import Input from '../UI/Input'
+import Modal from '../UI/Modal'
 import styles from './EditReview.module.css'
 
 // to do
 // add validation handling for edit form
 const EditReview = props => {
+    const [error, setError] = useState()
+
+    const handleError = () => {
+        setError(null)
+    }
 
     const { 
         value: album, 
-        // validValue: validAlbum, 
-        // hasError: invalidAlbum, 
+        validValue: validAlbum, 
+        hasError: invalidAlbum, 
         handleValueChange: handleAlbumChange, 
         handleValueBlur: handleAlbumBlur, 
-        // reset: resetAlbum 
     } = useInput(value => value.trim() !== '', props.selectedReview.album)
 
     const { 
         value: artist, 
-        // validValue: validArtist, 
-        // hasError: invalidArtist, 
+        validValue: validArtist, 
+        hasError: invalidArtist, 
         handleValueChange: handleArtistChange, 
         handleValueBlur: handleArtistBlur, 
-        // reset: resetArtist 
     } = useInput(value => value.trim() !== '', props.selectedReview.artist)
 
     const { 
         value: rating, 
-        // validValue: validRating, 
-        // hasError: invalidRating, 
+        validValue: validRating, 
+        hasError: invalidRating, 
         handleValueChange: handleRatingChange, 
         handleValueBlur: handleRatingBlur, 
-        // reset: resetRating 
     } = useInput(value => value.trim() !== '', props.selectedReview.rating)
 
     const { 
         value: date, 
-        // validValue: validDate, 
-        // hasError: invalidDate, 
+        validValue: validDate, 
+        hasError: invalidDate, 
         handleValueChange: handleDateChange, 
-        handleValueBlur: handleDateBlur, 
-        // reset: resetDate 
+        handleValueBlur: handleDateBlur,  
     } = useInput(value => value.trim() !== '', props.selectedReview.date)
 
     const { 
         value: text, 
-        // validValue: validText, 
-        // hasError: invalidText, 
+        validValue: validText, 
+        hasError: invalidText, 
         handleValueChange: handleTextChange, 
         handleValueBlur: handleTextBlur, 
-        // reset: resetText 
     } = useInput(value => value.trim() !== '', props.selectedReview.text)
 
     const today = () => {
@@ -61,6 +63,46 @@ const EditReview = props => {
 
     const handleSubmit = event => {
         event.preventDefault()
+
+        if (!validArtist) {
+            setError({
+                title: 'You missed a spot!',
+                message: 'Please enter an artist.'
+            })
+            return
+        }
+
+        if (!validAlbum) {
+            setError({
+                title: 'You missed a spot!',
+                message: 'Please enter an album.'
+            })
+            return
+        }
+
+        if (!validRating) {
+            setError({
+                title: 'You missed a spot!',
+                message: 'Please enter a rating.'
+            })
+            return
+        }
+
+        if (!validDate) {
+            setError({
+                title: 'You missed a spot!',
+                message: 'Please enter a date.'
+            })
+            return
+        }
+
+        if (!validText) {
+            setError({
+                title: 'You missed a spot!',
+                message: 'Please enter a text.'
+            })
+            return
+        }
 
         const reviewData = {
             id: props.selectedReview.id,
@@ -85,30 +127,32 @@ const EditReview = props => {
     }
 
     return(
-        <form id='editForm' onSubmit={handleSubmit}>
-            <Card className={styles['show-review']}>
-                <p className={styles['user-info']}>Editing Review by {props.selectedReview.user}</p>
-                <hr className={styles.rounded} />
-                <div className={styles['image-container']}>
-                    <img className={styles.image} alt={props.selectedReview.album} src={props.selectedReview.art[1].url} />
-                </div>
-                <div className={styles['album-info']}>
-                    <Input id="album" type='text' value={album} onChange={handleAlbumChange} onBlur={handleAlbumBlur} size='10' />
-                    <h3>by</h3>
-                    <Input id="artist" type='text' value={artist} onChange={handleArtistChange} onBlur={handleArtistBlur} />
-                </div>
-                <div className={styles['rating-info']}>
-                    <Input id="rating" type='number' value={rating} onChange={handleRatingChange} onBlur={handleRatingBlur} />
-                    <h4>/10</h4>
-                </div>
-                <div className={styles['album-info']}>
-                    <p className={styles['user-info']}>Listened on</p>
-                    <Input id='date' type='date' max={today()} value={date} onChange={handleDateChange} onBlur={handleDateBlur} />
-                </div>
-                <textarea id='text' name='text' rows='10' cols='90' value={text} onChange={handleTextChange} onBlur={handleTextBlur}></textarea>
-            </Card>
-        </form>
-
+        <>
+            {error && <Modal title={error.title} message={error.message} handleAction={handleError} />}
+            <form id='editForm' onSubmit={handleSubmit}>
+                <Card className={styles['show-review']}>
+                    <p className={styles['user-info']}>Editing Review by {props.selectedReview.user}</p>
+                    <hr className={styles.rounded} />
+                    <div className={styles['image-container']}>
+                        <img className={styles.image} alt={props.selectedReview.album} src={props.selectedReview.art[1].url} />
+                    </div>
+                    <div className={styles['album-info']}>
+                        <Input id="album" type='text' value={album} onChange={handleAlbumChange} onBlur={handleAlbumBlur} isValid={!invalidArtist} size='15' />
+                        <h3>by</h3>
+                        <Input id="artist" type='text' value={artist} onChange={handleArtistChange} onBlur={handleArtistBlur} isValid={!invalidArtist} />
+                    </div>
+                    <div className={styles['rating-info']}>
+                        <Input id="rating" type='number' value={rating} onChange={handleRatingChange} onBlur={handleRatingBlur} isValid={!invalidArtist} />
+                        <h4>/10</h4>
+                    </div>
+                    <div className={styles['album-info']}>
+                        <p className={styles['user-info']}>Listened on</p>
+                        <Input id='date' type='date' max={today()} value={date} onChange={handleDateChange} onBlur={handleDateBlur} isValid={!invalidArtist} />
+                    </div>
+                    <textarea id='text' name='text' rows='10' cols='90' value={text} onChange={handleTextChange} onBlur={handleTextBlur} isValid={!invalidArtist}></textarea>
+                </Card>
+            </form>
+        </>
     )
 }
 
