755109496128f36be68ce3c694bb13a088abe573
ah-james <11ahj11@gmail.com>
2023-01-13 21:31:38 -0500

    fleshing out add album form

--- src/components/NewReview/FindAlbum.js
+++ src/components/NewReview/FindAlbum.js
@@ -1,19 +1,25 @@
+import useInput from "../../hooks/use-input"
 import Button from "../UI/Button"
 import Input from "../UI/Input"
 
 
 const FindAlbum = props => {
 
+    const {
+        value: album,
+        handleValueChange: handleAlbumChange,
+    } = useInput(value => value.trim() !== '')
+
     const handleFoundAlbum = event => {
         event.preventDefault()
 
-        props.handleClick()
+        props.handleClick(album)
     }
 
     return(
         <>
             <form onSubmit={handleFoundAlbum}>
-                <Input id="album" type='text' label='Add an Album' />
+                <Input id="album" type='text' label='Add an Album' value={album} onChange={handleAlbumChange} />
                 <Button>Find Album</Button>
             </form>
         </>
