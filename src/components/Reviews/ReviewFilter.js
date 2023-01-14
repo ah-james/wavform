38c27366f00156798d9515c324194d90f0c6e477
ah-james <11ahj11@gmail.com>
2023-01-14 13:24:28 -0500

    cleaning up ReviewFilter and Reviews components

--- src/components/Reviews/ReviewFilter.js
+++ src/components/Reviews/ReviewFilter.js
@@ -4,43 +4,11 @@ import styles from './ReviewFilter.module.css'
 // import Button from '../UI/Button'
 
 const ReviewFilter = props => {
-    // const yearsArray = []
-
-    // const handleChange = (event) => {
-    //     props.handleFilterChange(event.target.value)
-    // }
-
-    // props.reviews.forEach(review => {
-    //     const date = new Date(review.date)
-    //     const year = date.getFullYear()
-    //     yearsArray.push(year)
-    // })
-
-    // const yearsSet = [...new Set(yearsArray)]
-
-    // let dynamicOptions = yearsSet.map(year => {
-    //     return <option key={year} value={`${year}`}>{year}</option>
-    // })
-
-    // let filter =
-    // <select value={props.selectedYear} onChange={handleChange}>
-    //     <option value=''>Select a Year</option>
-    //     {dynamicOptions}
-    // </select>
 
     return (
-        // <div className={styles['review-filter']}>
-        //     <div className={styles['review-filter-control']}>
-        //         <label>{props.showFilter ? 'Filter or Sort by Year' : 'Sort by Year'}</label>
-        //         <Button handleClick={props.changeSort}>Sort {props.ascending ? 'Descending' : 'Ascending'}</Button>
-        //         {/* <Button handleClick={years} /> */}
-        //         {props.showFilter && filter}
-        //     </div>
-        // </div>
-
-        <div>
+        <>
             <input className={styles['review-filter-control']} type='text' placeholder='search for an album' onChange={props.handleFilterChange}></input>
-        </div>
+        </>
     )
 }
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
 
 import styles from './Reviews.module.css'
 
@@ -9,50 +8,18 @@ import ReviewsList from './ReviewsList';
 import ReviewsChart from './ReviewsChart';
 
 // to do
-// add search bar
+// style search bar
 const Reviews = props => {
-    // const [filteredYear, setFilteredYear] = useState('')
     const [search, setSearch] = useState('')
-
-    // const navigate = useNavigate()
-    // const location = useLocation()
-
-    // const queryParams = new URLSearchParams(location.search)
-
-    // const isAscending = queryParams.get('sort') === 'asc'
-
-    // const handleFilterChange = selectedYear => {
-    //     setFilteredYear(selectedYear)
-    // }
 
     const handleFilterChange = event => {
         setSearch(event.target.value)
     }
 
-    // const reviews = props.reviews.filter(review => {
-    //     const date = new Date(review.date)
-    //     if (filteredYear === '') {
-    //         return props.reviews
-    //     }
-    //     return date.getFullYear().toString() === filteredYear
-    // })
-
     const filteredReviews = search.length === 0 ? props.reviews :
         props.reviews.filter(review => 
             review.album.toLowerCase().includes(search.toLowerCase())
     )
-
-    // const sortReviews = reviews.sort((a, b) => {
-    //     if (isAscending) {
-    //         return new Date(a.date) - new Date(b.date)
-    //     } else {
-    //         return new Date(b.date) - new Date(a.date)
-    //     }
-    // })
-
-    // const changeSort = () => {
-    //     navigate(`?sort=${isAscending ? 'desc' : 'asc'}`)
-    // }
 
     let chart =
     <div>
