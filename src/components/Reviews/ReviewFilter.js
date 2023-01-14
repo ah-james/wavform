bf74f48dac993adf4996426335c7f995e41a3e38
ah-james <11ahj11@gmail.com>
2023-01-14 13:23:00 -0500

    changing review filter from year etc to autosort search input

--- src/components/Reviews/ReviewFilter.js
+++ src/components/Reviews/ReviewFilter.js
@@ -1,41 +1,45 @@
 import React from 'react'
 
 import styles from './ReviewFilter.module.css'
-import Button from '../UI/Button'
+// import Button from '../UI/Button'
 
 const ReviewFilter = props => {
-    const yearsArray = []
+    // const yearsArray = []
 
-    const handleChange = (event) => {
-        props.handleFilterChange(event.target.value)
-    }
+    // const handleChange = (event) => {
+    //     props.handleFilterChange(event.target.value)
+    // }
 
-    props.reviews.forEach(review => {
-        const date = new Date(review.date)
-        const year = date.getFullYear()
-        yearsArray.push(year)
-    })
+    // props.reviews.forEach(review => {
+    //     const date = new Date(review.date)
+    //     const year = date.getFullYear()
+    //     yearsArray.push(year)
+    // })
 
-    const yearsSet = [...new Set(yearsArray)]
+    // const yearsSet = [...new Set(yearsArray)]
 
-    let dynamicOptions = yearsSet.map(year => {
-        return <option key={year} value={`${year}`}>{year}</option>
-    })
+    // let dynamicOptions = yearsSet.map(year => {
+    //     return <option key={year} value={`${year}`}>{year}</option>
+    // })
 
-    let filter =
-    <select value={props.selectedYear} onChange={handleChange}>
-        <option value=''>Select a Year</option>
-        {dynamicOptions}
-    </select>
+    // let filter =
+    // <select value={props.selectedYear} onChange={handleChange}>
+    //     <option value=''>Select a Year</option>
+    //     {dynamicOptions}
+    // </select>
 
     return (
-        <div className={styles['review-filter']}>
-            <div className={styles['review-filter-control']}>
-                <label>{props.showFilter ? 'Filter or Sort by Year' : 'Sort by Year'}</label>
-                <Button handleClick={props.changeSort}>Sort {props.ascending ? 'Descending' : 'Ascending'}</Button>
-                {/* <Button handleClick={years} /> */}
-                {props.showFilter && filter}
-            </div>
+        // <div className={styles['review-filter']}>
+        //     <div className={styles['review-filter-control']}>
+        //         <label>{props.showFilter ? 'Filter or Sort by Year' : 'Sort by Year'}</label>
+        //         <Button handleClick={props.changeSort}>Sort {props.ascending ? 'Descending' : 'Ascending'}</Button>
+        //         {/* <Button handleClick={years} /> */}
+        //         {props.showFilter && filter}
+        //     </div>
+        // </div>
+
+        <div>
+            <input className={styles['review-filter-control']} type='text' placeholder='search for an album' onChange={props.handleFilterChange}></input>
         </div>
     )
 }
