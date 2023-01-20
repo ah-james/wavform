76f0fb719b6809589ee6ff875e6ea8515eae9dc6
ah-james <11ahj11@gmail.com>
2023-01-20 15:42:46 -0500

    adjusted ChartBar to show years

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,13 +3,19 @@ import styles from './Chart.module.css'
 
 const Chart = props => {
     // get array of the values from all 12 months in AlbumsChart.js with .map
-    const yearValues = props.dataPoints.map(dataPoint => dataPoint.value)
+    const yearValues = Object.values(props.dataPoints)
     // find biggest value across months with Math.max
     const maxValue = Math.max(...yearValues)
 
+    let array = []
+
+    for(const [key, value] of Object.entries(props.dataPoints)) {
+        array.push({label: key, value: value})
+    }
+
     return (
         <div className={styles.chart}>
-            {props.dataPoints.map((dataPoint) => (
+            {array.map((dataPoint) => (
                 <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={maxValue} label={dataPoint.label} />
             ))}
         </div>
