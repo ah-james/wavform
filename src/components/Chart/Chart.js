c2530175a86088581ebfefc2f3fe6a08c6e5b686
ah-james <11ahj11@gmail.com>
2023-01-20 14:47:55 -0500

    starting work on changing Chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,9 +3,9 @@ import styles from './Chart.module.css'
 
 const Chart = props => {
     // get array of the values from all 12 months in AlbumsChart.js with .map
-    const monthValues = props.dataPoints.map(dataPoint => dataPoint.value)
+    const yearValues = props.dataPoints.map(dataPoint => dataPoint.value)
     // find biggest value across months with Math.max
-    const maxValue = Math.max(...monthValues)
+    const maxValue = Math.max(...yearValues)
 
     return (
         <div className={styles.chart}>
--- src/components/Reviews/ReviewsChart.js
+++ src/components/Reviews/ReviewsChart.js
@@ -3,26 +3,27 @@ import Chart from "../Chart/Chart"
 const ReviewsChart = props => {
     // chart data points variable, array containing objects of chart label for each month, value: 0
     const dataPoints = [
-        { label: 'Jan', value: 0 },
-        { label: 'Feb', value: 0 },
-        { label: 'Mar', value: 0 },
-        { label: 'Apr', value: 0 },
-        { label: 'May', value: 0 },
-        { label: 'Jun', value: 0 },
-        { label: 'Jul', value: 0 },
-        { label: 'Aug', value: 0 },
-        { label: 'Sep', value: 0 },
-        { label: 'Oct', value: 0 },
-        { label: 'Nov', value: 0 },
-        { label: 'Dec', value: 0 }
+        // { label: 'Jan', value: 0 },
+        // { label: 'Feb', value: 0 },
+        // { label: 'Mar', value: 0 },
+        // { label: 'Apr', value: 0 },
+        // { label: 'May', value: 0 },
+        // { label: 'Jun', value: 0 },
+        // { label: 'Jul', value: 0 },
+        // { label: 'Aug', value: 0 },
+        // { label: 'Sep', value: 0 },
+        // { label: 'Oct', value: 0 },
+        // { label: 'Nov', value: 0 },
+        // { label: 'Dec', value: 0 }
     ]
 
     // for loop through props.expenses from filteredAlbums in Albums.js
     // update value of each month in data points to reflect number of albums from that month
     for (const review of props.reviews) {
         const date = new Date(review.date)
-        const albumMonth = date.getMonth()
-        dataPoints[albumMonth].value += 1
+        const albumYear = date.getFullYear()
+        // dataPoints[albumYear].value += 1
+        dataPoints.push({ label: albumYear, value: 1 })
     }
 
     // return Chart component
