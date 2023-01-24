73f479be0710a5b4e36875eeb04c25de463ae4df
ah-james <11ahj11@gmail.com>
2023-01-24 15:44:12 -0500

    moving logout logic to Header container

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,31 @@
 // libraries
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 // components
 import Nav from "./Nav"
 // styling
 import styles from './Header.module.css'
+import { useDispatch } from 'react-redux'
+import { authActions } from '../../store/reducers/auth-slice'
 
-const Header = props => {
+const Header = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const logout = (event) => {
+        event.preventDefault()
+        dispatch(authActions.setLoggedOut())
+        // remove token from local storage
+        localStorage.removeItem('token')
+        localStorage.removeItem('email')
+        navigate('/')
+    }
 
     return (
         <header className={styles['main-header']}>
             <Link to={'/'}>
                 <h1>Wavform</h1>
             </Link>
-            <Nav handleLogout={props.handleLogout} />
+            <Nav logout={logout} />
         </header>
     )
 }
--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,23 +1,12 @@
 // libraries
-import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 // redux store
-import { authActions } from '../../store/reducers/auth-slice'
 // styles
 import styles from './Nav.module.css'
 
-const Nav = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+const Nav = props => {
     // use NavLink from react-router-dom, prevents page from reloading and can set CSS class on active path
-    const logout = (event) => {
-        event.preventDefault()
-        dispatch(authActions.setLoggedOut())
-        // remove token from local storage
-        localStorage.removeItem('token')
-        localStorage.removeItem('email')
-        navigate('/')
-    }
 
     const loggedIn = useSelector((state) => {
         return state.auth.loggedIn
@@ -40,7 +29,7 @@ const Nav = () => {
         loginButton =
             <>
                 <li>
-                    <NavLink className={activeButton} to='/login' onClick={logout}>Logout</NavLink>
+                    <NavLink className={activeButton} to='/login' onClick={props.logout}>Logout</NavLink>
                 </li>
                 <li>
                     <NavLink className={activeButton} to='/'>Home</NavLink>
