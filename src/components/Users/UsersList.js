6b6b4d41675da1d4523e1f210459fe2420b3a503
ah-james <11ahj11@gmail.com>
2023-01-26 18:12:14 -0500

    styling users list

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,5 +1,7 @@
 // libraries
 import { Link } from "react-router-dom"
+// styles
+import styles from './UsersList.module.css'
 
 const UsersList = props => {
 
@@ -7,7 +9,7 @@ const UsersList = props => {
         <div>
             <ul>
                 {props.users.map((user) =>
-                    <li key={user}><Link to={`/user/${user}`}>{user}</Link></li>
+                    <li className={styles['user-link']} key={user}><Link to={`/user/${user}`}>{user}</Link></li>
                 )}
             </ul>
         </div>
