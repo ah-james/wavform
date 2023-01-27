b1cbacbc7af02afdebfd8220e5b9789172819f3d
ah-james <11ahj11@gmail.com>
2023-01-27 10:21:37 -0500

    displaying most recent reviews on Home page

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,4 +1,5 @@
 // libraries
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 // UI components
 import Button from '../components/UI/Button'
@@ -7,6 +8,9 @@ import styles from './Container.module.css'
 
 const HomeContainer = () => {
     const navigate = useNavigate()
+    const reviews = useSelector((state) => {
+        return state.reviews.albums.slice(-4)
+    })
 
     return (
         <div className={styles['center-text']}>
@@ -14,6 +18,11 @@ const HomeContainer = () => {
             <h2>Save those you haven't heard yet.</h2>
             <h2>Tell your friends what's good.</h2>
             <Button handleClick={() => navigate('/signup')}>Get Started - It's Free!</Button>
+            <h2>Recent Reviews</h2>
+            {reviews.map((review) => 
+                // console.log(review)
+                <img alt={review.album} src={review.art[1].url} />
+            )}
         </div>
     )
 }
