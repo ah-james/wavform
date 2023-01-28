2831f3d1af0cc422c62d963af2e1f9461c94bba5
ah-james <11ahj11@gmail.com>
2023-01-28 14:23:32 -0500

    making list of recent reviews into an unordered list

--- src/components/Home/RecentReviewsList.js
+++ src/components/Home/RecentReviewsList.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom"
 import styles from './RecentReviewsList.module.css'
 
 const RecentReviewsList = props => {
-    return(
+    return (
         <div className={styles.recent}>
             <h2>Recent Reviews</h2>
-            <div className={styles.images}>
-                {props.reviews.map((review) => 
-                    <Link key={review.id} to={`/reviews/${review.id}`} state={{ creator: review.user }}><img alt={review.album} src={review.art[1].url} /></Link>
+            <ul className={styles.images}>
+                {props.reviews.map((review) =>
+                    <li>
+                        <Link key={review.id} to={`/reviews/${review.id}`} state={{ creator: review.user }}><img alt={review.album} src={review.art[1].url} /></Link>
+                    </li>
                 )}
-            </div>
+            </ul>
         </div>
     )
 }
