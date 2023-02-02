22a76f8733aead9b0314c7bc99f13a5371bbce1d
Christian Anderson <somecallmehans@gmail.com>
2023-02-01 21:10:20 -0500

    just some good old fashioned shenanigans

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,27 +1,31 @@
 // chart components
-import ChartBar from './ChartBar'
+import ChartBar from "./ChartBar";
 // styles
-import styles from './Chart.module.css'
+import styles from "./Chart.module.css";
 
-const Chart = props => {
-    // get array of the values from all 12 months in AlbumsChart.js with .map
-    const yearValues = Object.values(props.dataPoints)
-    // find biggest value across months with Math.max
-    const maxValue = Math.max(...yearValues)
+const Chart = ({ dataPoints }) => {
+  // get array of the values from all 12 months in AlbumsChart.js with .map
+  const yearValues = Object.values(dataPoints);
+  // find biggest value across months with Math.max
+  const maxValue = Math.max(...yearValues);
 
-    let array = []
+  const dataPointArray = Object.entries(dataPoints).map(([k, v]) => ({
+    label: k,
+    value: v,
+  }));
 
-    for(const [key, value] of Object.entries(props.dataPoints)) {
-        array.push({label: key, value: value})
-    }
+  return (
+    <div className={styles.chart}>
+      {dataPointArray.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={maxValue}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.chart}>
-            {array.map((dataPoint) => (
-                <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={maxValue} label={dataPoint.label} />
-            ))}
-        </div>
-    )
-}
-
-export default Chart
+export default Chart;
