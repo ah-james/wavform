b9b9f4e9f5ae202844353ab6c67e1e4c869cfd9e
ah-james <11ahj11@gmail.com>
2023-02-09 13:22:12 -0500

    finishing text inside overlay

--- src/components/Home/RecentReviewsList.js
+++ src/components/Home/RecentReviewsList.js
@@ -13,7 +13,13 @@ const RecentReviewsList = ({ reviews }) => {
                         <li className={styles.container} key={review.id} >
                             <Link to={`/reviews/${review.id}`} state={{ creator: review.user }}>
                                 <img alt={review.album} src={review.art[1].url} className={styles.image} />
-                                <div className={styles.overlay}>{review.album}</div>
+                                <div className={styles.overlay}>
+                                    <div className={styles.text}>
+                                        <h2>{review.artist}</h2>
+                                        <h3>{review.album}</h3>
+                                        <p>{review.rating}/10</p>
+                                    </div>
+                                </div>
                             </Link>
                         </li>
                     )}
