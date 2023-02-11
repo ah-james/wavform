06b5d0b5a8d970341fc5610e567411a56460236b
ah-james <11ahj11@gmail.com>
2023-02-11 13:53:58 -0500

    adjusting autofill to only fire when query has something in it

--- src/components/NewReview/FindAlbum.js
+++ src/components/NewReview/FindAlbum.js
@@ -14,22 +14,24 @@ const FindAlbum = ({ handleClick }) => {
         return state.spotify.accessToken
     })
 
-    const getAlbumOptions = async query => {
-        const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=album&limit=5`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
+    const getAlbumOptions = async event => {
+        if (query.length > 0) {
+            const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=album&limit=5`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error(`Couldn't fetch albums data`)
             }
-        })
 
-        if (!response.ok) {
-            throw new Error(`Couldn't fetch albums data`)
+            const data = await response.json()
+
+            setOptions(data.albums.items)
         }
-
-        const data = await response.json()
-
-        setOptions(data.albums.items)
     }
 
     const handleFoundAlbum = event => {
@@ -40,7 +42,7 @@ const FindAlbum = ({ handleClick }) => {
     return (
         <>
             <form onSubmit={handleFoundAlbum}>
-                <Input list="albums" id="album" type='text' placeholder='Add an Album' value={query} onChange={e => { setQuery(e.target.value); getAlbumOptions(query) }} />
+                <Input list="albums" type='text' placeholder='Add an Album' value={query} onChange={(e) => { setQuery(e.target.value); getAlbumOptions(query) }} />
                 <datalist id='albums'>
                     {query.length > 0 && options?.map((album, id) => {
                         return <option key={id}>{album.name} by {album.artists[0].name}</option>
