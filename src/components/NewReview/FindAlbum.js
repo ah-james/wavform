5ae9208677c122aa6a8b1985fed77522e078da16
ah-james <11ahj11@gmail.com>
2023-02-18 10:39:36 -0500

    debounce function for getAlbumOptions

--- src/components/NewReview/FindAlbum.js
+++ src/components/NewReview/FindAlbum.js
@@ -1,6 +1,6 @@
 // libraries
 import PropTypes from "prop-types";
-import { useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { useSelector } from "react-redux"
 // UI components
 import Button from "../UI/Button"
@@ -16,7 +16,8 @@ const FindAlbum = ({ handleClick }) => {
         return state.spotify.accessToken
     })
 
-    const getAlbumOptions = async () => {
+    const getAlbumOptions = useCallback (async (event) => {
+        setQuery(event.target.value)
         if (query.length > 0) {
             const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=album&limit=5`, {
                 method: 'GET',
@@ -34,7 +35,22 @@ const FindAlbum = ({ handleClick }) => {
 
             setOptions(data.albums.items)
         }
+    }, [query, accessToken])
+
+    // use debounce function to avoid making too many calls to Spotify API
+    const debounce = (callback, wait) => {
+        let timerId;
+        return function (...args) {
+            const context = this;
+            if (timerId) clearTimeout(timerId)
+            timerId = setTimeout(() => {
+                timerId = null
+                callback.apply(context, args)
+            }, wait);
+        };
     }
+
+    const debouncedResults = useMemo(() => debounce(getAlbumOptions, 300), [getAlbumOptions]);
 
     const handleFoundAlbum = event => {
         event.preventDefault()
@@ -44,7 +60,7 @@ const FindAlbum = ({ handleClick }) => {
     return (
         <>
             <form onSubmit={handleFoundAlbum}>
-                <Input list="albums" type='text' placeholder='Add an Album' value={query} onChange={(e) => { setQuery(e.target.value); getAlbumOptions(query) }} />
+                <Input list="albums" type='text' placeholder='Add an Album' onChange={debouncedResults} />
                 <FindAlbumDatalist query={query} options={options} />
                 <Button>Find Album</Button>
             </form>
