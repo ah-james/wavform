f2a0e632cf5079ee4a5194fcd97aad3167dac92c
ah-james <11ahj11@gmail.com>
2023-04-11 13:49:29 -0400

    comments text change handling

--- src/components/ShowReview/CommentsForm.js
+++ src/components/ShowReview/CommentsForm.js
@@ -1,15 +1,33 @@
+import { useState } from 'react'
 // styling
 import styles from './CommentsForm.module.css'
 
 const CommentsForm = props => {
+    const [text, setText] = useState('')
+    const [comments, setComments] = useState({})
+
+    const handleClick = () => {
+        setComments(text)
+    }
+
+    const handleTextChange = (event) => {
+        setText(event.target.value)
+    }
 
     return (
-        <div className={styles.comments}>
-            <p className={styles['user-info']}>Comments</p>
-            <hr className={styles.rounded} />
-            <textarea id='text' name='text' rows='4' cols='40' />
-            <button>Post</button>
-        </div>
+        <>
+            <div className={styles.comment_form}>
+                <p className={styles['user-info']}>Comments</p>
+                <hr className={styles.rounded} />
+                <textarea id='text' name='text' rows='4' cols='40' value={text} onChange={handleTextChange} />
+                <button onClick={handleClick}>Post</button>
+            </div>
+            <div className={styles.comments}>
+                {comments.map = (comment) => {
+                    <p>{comment}</p>
+                }}
+            </div>
+        </>
     )
 }
 
