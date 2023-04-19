44abaaded613752a32d958102e50ce695a85c0f2
ah-james <11ahj11@gmail.com>
2023-04-19 14:29:09 -0400

    connecting comments to store index

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import reviewSlice from './reducers/reviews-slice'
 import authSlice from './reducers/auth-slice'
 import spotifySlice from './reducers/spotify-slice'
+import commentSlice from './reducers/comments-slice'
 
 const store = configureStore({
     reducer: {
         reviews: reviewSlice.reducer,
         auth: authSlice.reducer,
-        spotify: spotifySlice.reducer
+        spotify: spotifySlice.reducer,
+        comments: commentSlice.reducer,
     }
 })
 
