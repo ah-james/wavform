7f005c39105d4414c41173aa9c0262d9cee7b94b
ah-james <11ahj11@gmail.com>
2023-04-24 15:38:49 -0400

    removing username being passed from ShowReview component to CommentsForm component

--- src/components/ShowReview/ShowReview.js
+++ src/components/ShowReview/ShowReview.js
@@ -34,7 +34,7 @@ const ShowReview = ({ selectedReview }) => {
             <p>{selectedReview.rating}/10</p>
             <p className={styles['listened-info']}>Listened on {selectedReview.date}</p>
             <p>{selectedReview.text}</p>
-            <CommentsForm userName={selectedReview.user} id={selectedReview.id}/>
+            <CommentsForm id={selectedReview.id} />
         </Card>
     )
 }
