c1d6f5ef5a9a817de7ffad21ea8ef66c03113d03
ah-james <11ahj11@gmail.com>
2023-04-25 16:44:22 -0400

    adding link to user's profile on click in comment

--- src/components/ShowReview/CommentItem.js
+++ src/components/ShowReview/CommentItem.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 import styles from './CommentItem.module.css'
 
 const CommentItem = ({ comment }) => {
@@ -5,7 +7,7 @@ const CommentItem = ({ comment }) => {
     return (
         <div key={comment.id}>
             <li className={styles.comment} >
-                <p className={styles.bold}>{comment.userName}</p>
+                <p className={styles.bold}><Link to={`/user/${comment.userName}`} >{comment.userName}</Link></p>
                 <div className={styles.text}>
                     <p>{comment.text}</p>
                 </div>
