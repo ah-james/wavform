afb56cbbbb220d7d5739d42a1b42f2c68538d5a5
ah-james <11ahj11@gmail.com>
2023-05-23 14:57:13 -0400

    adding styling to AlbumReviewsList

--- src/components/Album/AlbumReviewsList.js
+++ src/components/Album/AlbumReviewsList.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import styles from './AlbumReviewsList.module.css'
 
 const AlbumReviewsList = ({review}) => {
 
     return(
-        <Link to={`/user/${review.user}`} key={review.id}>
+        <Link to={`/user/${review.user}`} key={review.id} class={styles['review-item']}>
             <li>{review.user}: {review.rating}/10</li>
         </Link>
     )
