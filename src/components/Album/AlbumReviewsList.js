cd884d4437dcea1c0d97c7a623ef62375acb42d1
ah-james <11ahj11@gmail.com>
2023-05-23 16:16:42 -0400

    switching AlbumReviewsList to link to specific review instead of user's profile page

--- src/components/Album/AlbumReviewsList.js
+++ src/components/Album/AlbumReviewsList.js
@@ -4,7 +4,7 @@ import styles from './AlbumReviewsList.module.css'
 const AlbumReviewsList = ({review}) => {
 
     return(
-        <Link to={`/user/${review.user}`} key={review.id} class={styles['review-item']}>
+        <Link to={`/reviews/${review.id}`} key={review.id} state={{ creator: review.user }} className={styles['review-item']}>
             <li>{review.user}: {review.rating}/10</li>
         </Link>
     )
