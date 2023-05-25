e0c9f4b630ac4d9639c76a12fb75978c6c23b63e
ah-james <11ahj11@gmail.com>
2023-05-25 17:01:36 -0400

    building out AlbumReviewsList

--- src/components/Album/AlbumReviewsList.js
+++ src/components/Album/AlbumReviewsList.js
@@ -3,10 +3,16 @@ import styles from './AlbumReviewsList.module.css'
 
 const AlbumReviewsList = ({review}) => {
 
-    return(
-        <Link to={`/reviews/${review.id}`} key={review.id} state={{ creator: review.user }} className={styles['review-item']}>
-            <li>{review.user}: {review.rating}/10</li>
-        </Link>
+    return (
+        <div className={styles['review-item']}>
+            <p>Popular Reviews</p>
+            <hr className={styles.rounded} ></hr>
+            <ul>
+                    <Link to={`/reviews/${review.id}`} key={review.id} state={{ creator: review.user }} className={styles['review-info']}>
+                        <li>Review by {review.user} {review.rating}/10</li>
+                    </Link>
+            </ul>
+        </div>
     )
 }
 
