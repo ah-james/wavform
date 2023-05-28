8c96900a5638ddd3f7f0f1fe53a467e71b2139dc
ah-james <11ahj11@gmail.com>
2023-05-28 09:59:55 -0400

    breakline in AlbumReviewItem

--- src/components/Album/AlbumReviewItem.js
+++ src/components/Album/AlbumReviewItem.js
@@ -1,9 +1,12 @@
-
+import styles from './AlbumReviewIttem.module.css'
 
 const AlbumReviewItem = ({ review }) => {
 
     return (
-        <li>Review by <b>{review.user}</b> {review.rating}/10</li>
+        <>
+            <li>Review by <b>{review.user}</b> {review.rating}/10</li>
+            <hr className={styles.rounded} />
+        </>
     )
 }
 
